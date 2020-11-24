2b7d3735214029dee12840e179d05759b018d0c1
efe <zyuros@hotmail.com>
2020-11-24 15:11:27 -0300

    Moved TrueHeal to util. Fixed fog of war bug. Added data sent when learning an ability and refunding points

--- game/scripts/vscripts/util/util.lua
+++ game/scripts/vscripts/util/util.lua
@@ -434,3 +434,16 @@ function ApplyCallbackForUnitsInArea(caster, origin, radius, team, callback)
 
 	return enemies
 end
+
+function TrueHeal(base, heal, unit)
+    unit:SetHealthCustom(base + heal)
+
+    local new_treshold = unit:GetTreshold() + heal
+    if new_treshold > GameRules.GameMode.max_treshold then
+        unit:SetTreshold(GameRules.GameMode.max_treshold)
+    else
+        unit:SetTreshold(new_treshold)
+    end
+
+    SendOverheadHealMessage(unit, heal)
+end
