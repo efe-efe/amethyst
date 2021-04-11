e766519b5e1a8b86520cfb176b1eb2066451aba7
efe <zyuros@hotmail.com>
2021-04-11 13:51:27 -0400

    Improved invoker meteor

--- game/scripts/vscripts/abilities/heroes/invoker/invoker_extra/invoker_meteor_custom.lua
+++ game/scripts/vscripts/abilities/heroes/invoker/invoker_extra/invoker_meteor_custom.lua
@@ -1,107 +1,35 @@
 invoker_meteor_custom = class({})
 LinkLuaModifier("modifier_invoker_meteor_custom_thinker", "abilities/heroes/invoker/invoker_extra/modifier_invoker_meteor_custom_thinker", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_invoker_meteor_custom_impact_thinker", "abilities/heroes/invoker/invoker_extra/modifier_invoker_meteor_custom_impact_thinker", LUA_MODIFIER_MOTION_NONE)
 
 function invoker_meteor_custom:GetCastAnimationCustom()		return ACT_DOTA_CAST_CHAOS_METEOR end
-function invoker_meteor_custom:GetPlaybackRateOverride()	    return 0.3 end
-function invoker_meteor_custom:GetCastPointSpeed() 			return 0 end
-
-function invoker_meteor_custom:OnAbilityPhaseStart()
-	self.efx = ParticleManager:CreateParticle('particles/econ/items/wisp/wisp_relocate_channel_ti7.vpcf', PATTACH_ABSORIGIN_FOLLOW, self:GetCaster())
-	ParticleManager:SetParticleControlEnt(self.efx, 1, self:GetCaster(), PATTACH_ABSORIGIN_FOLLOW, 'attach_hitloc', self:GetCaster():GetAbsOrigin(), false)
-
-	return true
-end
-
-function invoker_meteor_custom:OnAbilityPhaseInterrupted()
-	DEFX(self.efx, true)
-end
+function invoker_meteor_custom:GetPlaybackRateOverride()	    return 1.0 end
+function invoker_meteor_custom:GetCastPointSpeed() 			return 80 end
 
 function invoker_meteor_custom:OnSpellStart()
-	if self.efx then
-		DEFX(self.efx, false)
-	end
-    local caster = self:GetCaster()
-    local origin = caster:GetAbsOrigin()
-    local point = CustomAbilities:GetCursorPosition(self)
-    local projectile_distance = self:GetCastRange(Vector(0,0,0), nil)
-    local final_point = ClampPosition(origin, point, projectile_distance, projectile_distance)
-	local damage = self:GetSpecialValueFor("ability_damage")
-	
-	local projectile_direction = Direction2D(origin, point)
-    local projectile_speed = self:GetSpecialValueFor("projectile_speed")
-    
-    local counter = 0
-	
-	CustomEntities:ProjectileAttack(caster, {
-		bTriggerCounters = false,
-		tProjectile = {
-			EffectName =			"particles/units/heroes/hero_invoker/invoker_chaos_meteor.vpcf",
-			vSpawnOrigin = 			origin + Vector(projectile_direction.x * 45, projectile_direction.y * 45, 0),
-			fDistance = 			projectile_distance,
-			fStartRadius =			self:GetSpecialValueFor("hitbox"),
-			Source = 				caster,
-			vVelocity = 			projectile_direction * projectile_speed,
-			UnitBehavior = 			PROJECTILES_NOTHING,
-			WallBehavior = 			PROJECTILES_NOTHING,
-			TreeBehavior = 			PROJECTILES_NOTHING,
-			GroundBehavior = 		PROJECTILES_NOTHING,
-			bIsReflectable =        false,
-			bIsDestructible =		false,
-			fGroundOffset = 		0,
-			UnitTest = function(_self, unit) return unit:GetUnitName() ~= "npc_dummy_unit" and not CustomEntities:Allies(_self.Source, unit) end,
-			OnUnitHit = function(_self, unit) 
-				local damage_table = {
-					victim = unit,
-					attacker = _self.Source,
-					damage = damage,
-					damage_type = DAMAGE_TYPE_PURE,
-				}
-				local distance = (final_point - unit:GetAbsOrigin()):Length2D()
-				ApplyDamage(damage_table)
-			end,
-			OnIntervalThink = function(_self, pos)
-				counter = counter + 1
-				if counter == 5 then
-					CreateModifierThinker(
-						caster, -- player source
-						self, -- ability source
-						"modifier_invoker_meteor_custom_thinker", -- modifier name
-						{ duration = 3.0 }, -- kv
-						pos,
-						caster:GetTeamNumber(),
-						false --bPhantomBlocker
-					)
-					counter = 0
-				end
-			end,
-			OnFinish = function(_self, pos)
-				EFX("particles/units/heroes/hero_invoker/invoker_chaos_meteor_crumble.vpcf", PATTACH_WORLDORIGIN, nil, {
-					cp0 = pos,
-					cp3 = pos + Vector(0, 0, 32),
-					release = true
-				})
-				EmitSoundOn("Hero_Invoker.ChaosMeteor.Impact", caster)
-				StopSoundOn("Hero_Invoker.ChaosMeteor.Loop", caster)
-			end,
-		}
-	})
-
+	local caster = self:GetCaster()
+	local origin = caster:GetAbsOrigin()
+	local point = ClampPosition(caster:GetAbsOrigin(), CustomAbilities:GetCursorPosition(self), self:GetCastRange(Vector(0,0,0), nil), self:GetSpecialValueFor("min_range"))
+		
+	CreateModifierThinker(
+		caster,
+		self,
+		"modifier_invoker_meteor_custom_impact_thinker",
+		{
+			x = origin.x,
+			y = origin.y
+		},
+		point,
+		caster:GetTeamNumber(),
+		false
+	)
 	self:PlayEffectsOnCast()
 end
 
-function invoker_meteor_custom:PlayEffectsOnFinish(pos)
-	local caster = self:GetCaster()
-
-	EFX('particles/units/heroes/hero_arc_warden/arc_warden_wraith_cast.vpcf', PATTACH_ABSORIGIN, caster, {
-		cp0 = pos,
-		cp1 = pos,
-		cp2 = pos,
-		release = true,
-	})
-end
-
 function invoker_meteor_custom:PlayEffectsOnCast()
-	EmitSoundOn("Hero_Invoker.ChaosMeteor.Loop", self:GetCaster())
+	EFX("particles/invoker/invoker_meteor_cast.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetCaster(), {
+		release = true
+	})
 	EmitSoundOn("Hero_Invoker.ChaosMeteor.Cast", self:GetCaster())
 end
 
---
+++ game/scripts/vscripts/abilities/heroes/invoker/invoker_extra/modifier_invoker_meteor_custom_impact_thinker.lua
@@ -0,0 +1,111 @@
+modifier_invoker_meteor_custom_impact_thinker = class({})
+
+function modifier_invoker_meteor_custom_impact_thinker:OnCreated(params)
+    self.caster = self:GetCaster()
+    self.origin = self:GetParent():GetAbsOrigin()
+    self.radius = self:GetAbility():GetSpecialValueFor("radius")
+    self.projectile_speed = self:GetAbility():GetSpecialValueFor("projectile_speed")
+
+    if IsServer() then
+        local caster_origin = Vector(params.x, params.y)
+        self.direction = Direction2D(caster_origin, self.origin)
+        print((caster_origin - self.origin):Length2D())
+        self.damage_table = {
+            attacker = self.caster,
+            damage = self:GetAbility():GetSpecialValueFor("ability_damage"),
+            damage_type = DAMAGE_TYPE_PURE,
+        }
+
+        local delay_time = 0.5--self:GetAbility():GetSpecialValueFor('delay_time')
+        CreateTimedRadiusMarker(self.caster, self.origin, self.radius, delay_time, 0.2, RADIUS_SCOPE_PUBLIC)
+        AddFOWViewer(self.caster:GetTeamNumber(), self.origin, self.radius, delay_time +  0.2, true)
+        self:StartIntervalThink(delay_time)
+
+        local end_point = (self.origin + self.direction * self.projectile_speed) - Vector(0, 0, 1000)
+        local efx = ParticleManager:CreateParticle("particles/units/heroes/hero_invoker/invoker_chaos_meteor_fly.vpcf", PATTACH_ABSORIGIN, self.caster)
+        ParticleManager:SetParticleControl(efx, 0, caster_origin + Vector(0, 0, 1000))
+        ParticleManager:SetParticleControl(efx, 1, end_point)
+        ParticleManager:SetParticleControl(efx, 2, Vector(1.0, 0, 0))
+
+	    EmitSoundOn("Hero_Invoker.ChaosMeteor.Loop", self:GetCaster())
+    end
+end
+
+function modifier_invoker_meteor_custom_impact_thinker:OnIntervalThink()
+    self:CreateMeteor()
+    self:Destroy()
+end
+
+function modifier_invoker_meteor_custom_impact_thinker:CreateMeteor()
+    local projectile_distance = 1200
+    local counter = 0
+
+    CustomEntities:ProjectileAttack(self.caster, {
+        bTriggerCounters = false,
+        tProjectile = {
+            EffectName =			"particles/units/heroes/hero_invoker/invoker_chaos_meteor.vpcf",
+            vSpawnOrigin = 			self.origin + Vector(self.direction.x * 45, self.direction.y * 45, 0),
+            fDistance = 			projectile_distance,
+            fStartRadius =			self:GetAbility():GetSpecialValueFor("hitbox"),
+            Source = 				self.caster,
+            vVelocity = 			self.direction * self.projectile_speed,
+            UnitBehavior = 			PROJECTILES_NOTHING,
+            WallBehavior = 			PROJECTILES_NOTHING,
+            TreeBehavior = 			PROJECTILES_NOTHING,
+            GroundBehavior = 		PROJECTILES_NOTHING,
+            bIsReflectable =        false,
+            bIsDestructible =		false,
+            fGroundOffset = 		0,
+            UnitTest = function(_self, unit) return unit:GetUnitName() ~= "npc_dummy_unit" and not CustomEntities:Allies(_self.Source, unit) end,
+            OnUnitHit = function(_self, unit) 
+                local damage_table = {
+                    victim = unit,
+                    attacker = _self.Source,
+                    damage = damage,
+                    damage_type = DAMAGE_TYPE_PURE,
+                }
+                ApplyDamage(damage_table)
+            end,
+            OnIntervalThink = function(_self, pos)
+                counter = counter + 1
+                if counter == 5 then
+                    CreateModifierThinker(
+                        self.caster, -- player source
+                        self, -- ability source
+                        "modifier_invoker_meteor_custom_thinker", -- modifier name
+                        { duration = 3.0 }, -- kv
+                        pos,
+                        self.caster:GetTeamNumber(),
+                        false --bPhantomBlocker
+                    )
+                    counter = 0
+                end
+            end,
+            OnFinish = function(_self, pos)
+                EFX("particles/units/heroes/hero_invoker/invoker_chaos_meteor_crumble.vpcf", PATTACH_WORLDORIGIN, nil, {
+                    cp0 = pos,
+                    cp3 = pos + Vector(0, 0, 32),
+                    release = true
+                })
+                EmitSoundOn("Hero_Invoker.ChaosMeteor.Impact", self.caster)
+                StopSoundOn("Hero_Invoker.ChaosMeteor.Loop", self.caster)
+            end,
+        }
+    })
+    EmitSoundOn("Hero_Invoker.ChaosMeteor.Impact", self.caster)
+
+    EFX("particles/econ/items/centaur/centaur_ti6/centaur_ti6_warstomp.vpcf", PATTACH_WORLDORIGIN, nil, {
+        cp0 = self.origin,
+        cp1 = Vector(self.radius, 0, 0), 
+        release = true
+    })
+end
+
+function modifier_invoker_meteor_custom_impact_thinker:PlayEffectsOnFinish(pos)
+	EFX('particles/units/heroes/hero_arc_warden/arc_warden_wraith_cast.vpcf', PATTACH_ABSORIGIN, self:GetCaster(), {
+		cp0 = pos,
+		cp1 = pos,
+		cp2 = pos,
+		release = true,
+	})
+end
