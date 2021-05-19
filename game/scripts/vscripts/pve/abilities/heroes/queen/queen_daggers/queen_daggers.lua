bf878b0b50ef8e21873b4a17bee9c29af8ed723e
efe <zyuros@hotmail.com>
2021-05-19 17:05:26 -0400

    Refactor on Queen Daggers spell

--- game/scripts/vscripts/pve/abilities/heroes/queen/queen_daggers/queen_daggers.lua
+++ game/scripts/vscripts/pve/abilities/heroes/queen/queen_daggers/queen_daggers.lua
@@ -11,10 +11,11 @@ function queen_daggers:OnSpellStart()
     local angle_diff = 360/count
     local vector = origin + Vector(1, 0, 0)
     local queen_attack = caster:FindAbilityByName("queen_attack")
+    local distance = queen_attack:GetCastRange(Vector(0,0,0), nil) * 2
 
     for i = 0, count - 1 do
         local location = RotatePosition(origin, QAngle(0, angle_diff * i, 0), vector)
-        queen_attack:LaunchProjectile(location, 600, queen_attack:GetCastRange(Vector(0,0,0), nil) * 2, 7)
+        queen_attack:LaunchProjectile(location, 600, distance, 7)
     end
 
     EmitSoundOn("Hero_QueenOfPain.ShadowStrike", caster)
