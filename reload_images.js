0caad8b8479d994375e6f76a223c3eb68aa59a92
efe <zyuros@hotmail.com>
2021-05-20 20:59:37 -0400

    Added node script to generate the image imports. Deleted unused images

---
+++ reload_images.js
@@ -0,0 +1,14 @@
+const testFolder = './content/panorama/images/custom_game';
+const fs = require('fs');
+
+fs.readdir(testFolder, (err, files) => {
+    if (err) throw err;
+    console.log('File is created successfully.');
+    files.forEach(file => {
+        const text = `<Image src="file://{images}/custom_game/${file}" />\n`;
+        fs.appendFile('image_names.txt', text, function (err) {
+            if (err) throw err;
+            console.log('File is created successfully.');
+        });
+    });
+});
