af8f04f61e5684f123e87b10fe68bb455aebc218
efe <zyuros@hotmail.com>
2021-05-23 19:27:22 -0400

    Fixed Coup de grace cast fast being removed on death

--- game/scripts/vscripts/modifiers/upgrades/modifier_upgrade_phantom_coup_cast_fast.lua
+++ game/scripts/vscripts/modifiers/upgrades/modifier_upgrade_phantom_coup_cast_fast.lua
@@ -1,1 +1,3 @@
 modifier_upgrade_phantom_coup_cast_fast = class({})
+function modifier_upgrade_phantom_dash_damage:RemoveOnDeath() return false end
+function modifier_upgrade_phantom_dash_damage:IsPurgable() return false end
