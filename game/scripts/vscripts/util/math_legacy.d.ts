e0dcbc89e0e26a1462a4e77d988e4d0a45eb716d
efe <zyuros@hotmail.com>
2021-05-30 20:10:22 -0400

    Fixed math legacy declaration files bug

--- game/scripts/vscripts/util/math_legacy.d.ts
+++ game/scripts/vscripts/util/math_legacy.d.ts
@@ -1,4 +1,4 @@
 declare function RotatePoint(this: void, origin: Vector, point: Vector, angle: number): Vector;
 declare function RandomIntWithExeption(this: void, iMinimum: number, iMaximum: number, iException: number): number;
 declare function NearestValue(this: void, table: any, value: number): number;
-declare function Clamp(this: void, iNumber: number, iMax: number, iMin: number): number;
+declare function Clamp(this: void, iNumber: number, iMax: number | undefined, iMin: number): number;
