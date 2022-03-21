644ec75a9f7f7263addd34785b35acdfaaa48fb3
efe <zyuros@hotmail.com>
2022-03-20 22:44:46 -0300

    AbilitiesMeta now is TS

--- game/scripts/vscripts/abilities_meta.lua
+++ game/scripts/vscripts/abilities_meta.lua
@@ -1,19 +1,28 @@
-if not AbilitiesMeta then
-    AbilitiesMeta = class({})
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+require("lualib_bundle");
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 10,["5"] = 11,["6"] = 13,["7"] = 13,["8"] = 13,["9"] = 13,["10"] = 13,["11"] = 14,["12"] = 15,["14"] = 15,["18"] = 14,["19"] = 16,["21"] = 16,["25"] = 14,["26"] = 14,["28"] = 20});
+abilitiesKv = LoadKeyValues("scripts/npc/npc_abilities_custom.txt")
+meta = {}
+for ____, ____value in ipairs(
+    __TS__ObjectEntries(abilitiesKv)
+) do
+    key = ____value[1]
+    ability = ____value[2]
+    meta[key] = {
+        targetingIndicator = (function(____lhs)
+            if ____lhs == nil then
+                return {}
+            else
+                return ____lhs
+            end
+        end)(ability.TargetingIndicator),
+        energyCost = (function(____lhs)
+            if ____lhs == nil then
+                return 0
+            else
+                return ____lhs
+            end
+        end)(ability.AbilityEnergyCost)
+    }
 end
-
-function AbilitiesMeta:Load()
-    local abilities_kv = LoadKeyValues("scripts/npc/npc_abilities_custom.txt")
-    local meta = {}
-
-    for key, ability in pairs(abilities_kv) do
-        meta[key] = {
-            targetingIndicator = ability.TargetingIndicator or {},
-            energyCost = ability.AbilityEnergyCost or 0,
-        }
-    end
-
-    CustomNetTables:SetTableValue("main", "abilities", meta)
-end
-
-AbilitiesMeta:Load()
+CustomNetTables:SetTableValue("main", "abilities", meta)
