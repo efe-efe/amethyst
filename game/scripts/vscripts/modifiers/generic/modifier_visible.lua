b443e482b731af0d968abcff4a0a8e700a8452d9
efe <fabian.urbina@usach.cl>
2023-05-22 23:01:49 -0400

    Modifier visible is hidden again

--- game/scripts/vscripts/modifiers/generic/modifier_visible.lua
+++ game/scripts/vscripts/modifiers/generic/modifier_visible.lua
@@ -1,6 +1,6 @@
 modifier_visible = class({})
 
-function modifier_visible:IsHidden()        return false end
+function modifier_visible:IsHidden()        return true end
 function modifier_visible:IsDebuff()        return	false	end
 function modifier_visible:IsPurgable() 		return	false	end
 function modifier_visible:IsPermanent()   return	false	end
